0bb19056ea19b4210011a940e311b1f2a7645a9b
jenaanalshayji <66732804+jenaanalshayji@users.noreply.github.com>
2020-06-13 23:06:16 +0300

    cw 1 done
    classwork 1 done

---
+++ CW-1-jenaan alshayji.playground/Contents.swift
@@ -0,0 +1,24 @@
+import UIKit
+
+//part 1
+let name = "jenaan"
+var hobby1 = "swimming"
+var hobby2 = "drawing"
+var age = 16
+var height = 160
+print(" my name is \(name),","im \(age) years old.","My height is \(height) cm,","and my hobbies are \(hobby1)","and \(hobby2)." )
+
+//part2
+var friendName = "rawaan"
+var friendAge = 16
+var friendHeight = 157
+var friendHobby1 = "swimming"
+var friendHobby2 = "singing"
+
+//comparing
+name != friendName
+age == friendAge
+height != friendHeight
+hobby1 == friendHobby1
+hobby2 != friendHobby2
+
